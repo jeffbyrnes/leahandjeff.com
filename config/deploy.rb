e9bd42d47c07dd5a5bb0a99a13ccfcd301281054
Jeff Byrnes <jeff@evertrue.com>
2014-03-11 22:16:49 -0400

    Correct :application variable in Cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@bitbucket.org:jeffbyrnes/thejeffbyrnes.com.git'
+set :application, 'jeffandleahswedding.com'
+set :repo_url, "git@bitbucket.org:jeffbyrnes/#{fetch(:application)}.git"
 
 set :deploy_to, "/srv/#{fetch(:application)}"
 
-server 'thejeffbyrnes.com', user: 'deploy', roles: %w{web app}
+server "#{fetch(:application)}", user: 'deploy', roles: %w{web app}
