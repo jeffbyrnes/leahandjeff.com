e1a33f6a6be163c9641c1a8ecf409cecd62cc941
Jeff Byrnes <jeff@evertrue.com>
2014-03-17 22:13:07 -0400

    Change URL to leahandjeff.com

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'jeffandleahswedding.com'
+set :application, 'leahandjeff.com'
 set :repo_url, "git@bitbucket.org:jeffbyrnes/#{fetch(:application)}.git"
 
 set :deploy_to, "/srv/#{fetch(:application)}"
