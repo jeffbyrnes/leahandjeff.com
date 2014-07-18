1ca518375ecdb42fdb8a13e20048f109abc19418
Jeff Byrnes <jeff@evertrue.com>
2014-07-18 10:27:29 -0400

    Fix Compass config after moving SCSS files

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = '/'
 path_to_theme = 'public'
 css_dir = "#{path_to_theme}/css"
-sass_dir = 'scss'
+sass_dir = "#{path_to_theme}/scss"
 images_dir = "#{path_to_theme}/img"
 javascripts_dir = "#{path_to_theme}/js"
 fonts_dir = "#{path_to_theme}/fonts"
